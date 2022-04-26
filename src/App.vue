953c55ee76c3b3b24ac31a2592c867dc7e42e156
Fidelis Guimarães <atosfiel@gmail.com>
2022-04-26 03:04:39 +0100

    hello word with vue app and i18n

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <h1>{{i18n.t("message.welcome")}}</h1>
+
+  <div class="locale-changer">
+    <select v-model="$i18n.locale">
+      <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
+    </select>
+  </div>
+
+  <p-input-text v-model="text" />
+  <p-button icon="pi pi-user" @click="greet" />{{i18n.t("message.greet")}}
+  <p-toast />
+  {{text}}
+  
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script >
+import { defineComponent, ref } from "vue";
+import { useToast } from 'primevue/usetoast';
+import { useI18n } from 'vue-i18n'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default defineComponent ({
+  setup(){
+    const toast = useToast();
+    const text = ref('');
+    const i18n = useI18n();
+    const greet = () => {
+      toast.add({severity: 'info', summary: i18n.t("message.hello"), detail: text});
+    }
+    return {
+      text,
+      greet,
+      i18n
+    }
   }
-}
+})
+
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,57 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+
+import Toast from 'primevue/toast'
+import ToastService from 'primevue/toastservice'
+
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+
+const app = createApp(App);
+
+const messages = {
+    en: {
+      message: {
+        welcome: 'Welcome',
+        greet: 'Greet',
+        hello: 'Hello'
+      }
+    },
+    pt: {
+      message: {
+        welcome: 'Seja bem vindo',
+        greet: 'Saudar',
+        hello: 'Olá'
+      }
+    },
+    ja: {
+        message: {
+          welcome: 'こんにちは、世界',
+          greet: '挨拶',
+          hello: 'こんにちは'
+        }
+      }
+  }
+
+const i18n = createI18n({
+    locale: 'pt', // set locale
+    fallbackLocale: 'en',
+    messages
+});
+
+app.use(PrimeVue);
+app.use(ToastService);
+app.use(i18n);
+
+app.component('p-button', Button);
+app.component('p-input-text', InputText);
+app.component('p-toast', Toast);
+
+app.mount('#app')
